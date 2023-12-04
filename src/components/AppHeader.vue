80441d37c9a5b245ef0717f8532428a60d15e2c0
MarcoGranata9 <granatam9900@gmail.com>
2023-12-04 14:03:12 +0100

    add: header timer

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,9 @@
 <script>
+import HeaderTimer from './HeaderTimer.vue';
+
 export default {
     data() {
-        return {
-
-        }
+        return {};
     },
     methods: {
         getImgPath(imgName) {
@@ -12,12 +12,14 @@ export default {
     },
     props: {
         headerProps: Object
-    }
+    },
+    components: { HeaderTimer }
 }
 </script>
 
 <template>
     <header>
+        <HeaderTimer/>
         <div class="container">
             <div class="wrapper">
                 <div class="logo">
@@ -46,13 +48,14 @@ export default {
 @use "../style/partials/variables" as *;
 
 header { 
-    height: 100px;
     .container {
         height: 100%
     }
     .wrapper {
         @include flex(space-between, center, row);
         height: 100%;
+        font-size: 1.1rem;
+        padding: 20px 0;
         ul {
             @include flex(space-between, center, row);
             gap: 1rem;
@@ -72,7 +75,8 @@ header {
     .nav {
         a{
             @include flex(center, center, row);
-            gap: 0.3rem;        
+            gap: 0.3rem;
+            color: #3b3b3b;        
         }
         i {
             font-size: 0.5rem
@@ -82,7 +86,7 @@ header {
     .social {
         a {
             font-size: 1.2rem;
-            color: $gray-cube;
+            color: $gray-icon;
         }
     }
 }
---
+++ src/components/HeaderTimer.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="header-top">
+        <div class="container">
+            <div class="timer-wrapper">
+                <p>Starts TOMORROW! Our biggest event of the year...</p>
+                <div class="timer"><i class="fa-regular fa-clock"></i> 00 : 00 : 00 : 00</div>
+                <button class="btn">Get ticket</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
+.header-top {
+    background-color: $gray-bg;
+    .container {
+        width: 50%;
+        max-width: 800px;
+        .timer-wrapper{
+            @include flex(space-between, center, row);
+            padding: 15px 0;
+            p{
+                color: #7d7d7d;
+                font-size: 0.9rem;
+            }
+            .timer {
+                color: #3f4369;
+                font-weight: bold;
+            }
+        }
+    }
+}
+</style>
